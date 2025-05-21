b256d210e47c11b5e4a457a1a823d1cd6b9e16f3
covy <junni802@naver.com>
2025-05-21 17:09:03 +0900

    feat: add DockerExampleController File

---
+++ src/main/java/covy/dockerexample/controller/DockerExampleController.java
@@ -0,0 +1,15 @@
+package covy.dockerexample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerExampleController {
+
+  @GetMapping("/")
+  public String exampleDocker() {
+    return "Hello World!!";
+  }
+
+}
